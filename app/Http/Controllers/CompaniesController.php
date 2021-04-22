94c6e2dff4d5406e0ed4fc8cbb26b3df0a222474
Krzysztof Musiał <niemusialem@gmail.com>
2021-04-22 02:38:36 +0200

    added views and controllers

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Company;
+use App\Models\Category;
+use App\Models\Location;
 
 class CompaniesController extends Controller
 {
     public function index(Request $request)
     {
-        return view('companies.index');
+        // List of values for select inputs in form
+        $locations = Location::get();
+        // Requested values for filtering the results
+        $location = $request->location;
+        // Number of items per page, used in pagination
+        $perPage = 12;
+        // Requested companies
+        $companies = Company::when($location, function ($query, $location) {
+                    return $query->where('location_id', '=', $location);
+                })
+                ->paginate($perPage);
+                
+        return view('companies.index', compact('companies', 'locations'));
     }
     // Display the specified resource.
     public function show(Company $company)
--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use App\Models\Location;
 
 class StudentsController extends Controller
 {
     public function index(Request $request)
     {
-        return view('students.index');
+        // List of values for select inputs in form
+        $locations = Location::get();
+        // Requested values for filtering the results
+        $location = $request->location;
+        // Number of items per page, used in pagination
+        $perPage = 12;
+        // Requested companies
+        $students = Student::when($location, function ($query, $location) {
+                    return $query->where('location_id', '=', $location);
+                })
+                ->paginate($perPage);
+                
+        return view('students.index', compact('students', 'locations'));
     }
     // Display the specified resource.
     public function show(Student $student)
