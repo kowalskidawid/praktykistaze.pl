145e605a130766e5daf8c2e96640cd994911f7d5
Krzysztof Musiał <niemusialem@gmail.com>
2021-04-28 19:41:40 +0200

    filtering results

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -15,11 +15,19 @@ class CompaniesController extends Controller
         $locations = Location::get();
         // Requested values for filtering the results
         $location = $request->location;
+        $city = $request->city;
+        $name = $request->name;
         // Number of items per page, used in pagination
         $perPage = 12;
         // Requested companies
         $companies = Company::when($location, function ($query, $location) {
                     return $query->where('location_id', '=', $location);
+                })
+                ->when($city, function ($query, $city) {
+                    return $query->where('city', 'LIKE', '%'.$city.'%');
+                })
+                ->when($name, function ($query, $name) {
+                    return $query->where('company_name', 'LIKE', '%'.$name.'%');
                 })
                 ->paginate($perPage);
                 
