8e23d1b4885c8b1670a20e00811273f2555a468b
Krzysztof Musiał <niemusialem@gmail.com>
2021-05-21 08:13:55 +0200

    dashboard menu changes

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,38 +7,37 @@
     {{-- Aside --}}
     <div class="flex flex-col space-y-4">
         <div class="p-4 bg-white border border-gray-200 rounded-lg flex flex-col space-y-4">
-            <ul class="flex flex-col space-y-2 whitespace-nowrap">
+            <ul class="flex flex-col whitespace-nowrap">
                 <li class="flex">
-                    <a href="{{ route('dashboard.index') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard') ? 'bg-blue-600 text-white' : ''}}">{{ __('Dashboard')}}</a>
+                    <a href="{{ route('dashboard.index') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Dashboard')}}</a>
+                </li>
+                <li class="my-2"><hr></li>
+                <li class="flex">
+                    <a href="{{ route('dashboard.settings') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard/settings') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Settings')}}</a>
                 </li>
                 <li class="flex">
-                    <a href="{{ route('dashboard.settings') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard/settings') ? 'bg-blue-600 text-white' : ''}}">{{ __('Settings')}}</a>
-                </li>
-                <li class="flex">
-                    <a href="{{ route('dashboard.profile') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard/profile') ? 'bg-blue-600 text-white' : ''}}">{{ __('Profile')}}</a>
+                    <a href="{{ route('dashboard.profile') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard/profile') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Profile')}}</a>
                 </li>
                 {{-- Student links --}}
                 @if(Auth::user()->roleCheck('student'))
                 <li class="flex">
-                    <a href="{{ route('dashboard.favourites') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard/favourites') ? 'bg-blue-600 text-white' : ''}}">{{ __('Favourites')}}</a>
+                    <a href="{{ route('dashboard.favourites') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard/favourites') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Favourites')}}</a>
                 </li>
                 <li class="flex">
-                    <a href="{{ route('dashboard.applications') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard/applications') ? 'bg-blue-600 text-white' : ''}}">{{ __('Applications')}}</a>
+                    <a href="{{ route('dashboard.applications') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard/applications') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Applications')}}</a>
                 </li>
                 @endif
                 @if(Auth::user()->roleCheck('company'))
                 {{-- Company links --}}
                 <li class="flex">
-                    <a href="{{ route('dashboard.offers') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard/offers') ? 'bg-blue-600 text-white' : ''}}">{{ __('Offers')}}</a>
+                    <a href="{{ route('dashboard.offers') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard/offers') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Offers')}}</a>
                 </li>
                 <li class="flex">
-                    <a href="{{ route('dashboard.applicants') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-600 hover:text-white {{ request()->is('dashboard/applicants') ? 'bg-blue-600 text-white' : ''}}">{{ __('Applicants')}}</a>
+                    <a href="{{ route('dashboard.applicants') }}" class="w-full text-sm font-medium px-4 py-2 rounded hover:bg-blue-100 {{ request()->is('dashboard/applicants') ? 'bg-blue-600 text-white hover:bg-blue-600' : ''}}">{{ __('Applicants')}}</a>
                 </li>
                 @endif
+                <li class="my-2"><hr></li>
                 {{-- Logout --}}
-                <li>
-                    <hr>
-                </li>
                 <li>
                     <form method="POST" action="{{ route('logout') }}" class="flex">
                         @csrf
