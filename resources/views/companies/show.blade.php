4a3b329af3dcbf4e29da432896d84d3e3391ffcf
Kprylutska <61788830+Kprylutska@users.noreply.github.com>
2021-05-25 15:24:15 +0200

    updated languages, added some links, updated latest offers

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -16,7 +16,7 @@
         <div class="flex flex-col space-y-4 whitespace-nowrap flex-shrink-0">
             @if (Auth::user() && Auth::user()->roleCheck('company') && Auth::user()->company->id == $company->id)
                 <a href="{{ route('dashboard.profile') }}" type="submit" class="w-full text-sm font-medium px-4 py-2 rounded border border-blue-600 bg-blue-600 text-white text-center hover:bg-blue-500">
-                    Edytuj profil
+                    {{ __('Edytuj profil')}}
                 </a>
             @endif
             <div class="p-4 bg-white border border-gray-200 rounded-lg flex flex-col space-y-8">
